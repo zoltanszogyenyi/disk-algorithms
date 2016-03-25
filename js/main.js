64e84be96c4711a731b65d416304a6fe5c4abfa1
Zoli Szogyenyi <zoltan.szogyenyi@gmail.com>
2016-03-25 15:37:51 +0200

    Added some input control

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-	
+	// if user presses enter
+	$('#diskMainInput').keypress(function(e) {
+		if(e.which == 13) {
+	        // generate span inside input
+	        var inputValue = $('#diskMainInput').val();
+
+	        $('.disk-values-for-chart').append('<button class="button large-button">' + inputValue + '</button>');
+	        $('#diskMainInput').val('')
+	    }
+	})
 })
