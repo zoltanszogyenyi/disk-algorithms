e2336bc686b0feb0cbdaf63b0e32708d25f55649
Zoli Szogyenyi <zoltan.szogyenyi@gmail.com>
2016-04-13 20:29:38 +0300

    Generating chart based on data.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
+	function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+	}
+
+
 	var values = [];
 	function addValue(val) {
 		values.push(val);
@@ -14,13 +24,33 @@ $(document).ready(function() {
 					addValue(inputValue);
 
 	        $('.disk-values-for-chart').append('<button class="button large-button">' + inputValue + '</button>');
-	        $('#diskMainInput').val('')
+	        $('#diskMainInput').val('');
 	    }
 	})
 
 	// if user generates charts
 	$('#generateResults').click(function() {
+		var startCilinders = $('#startCilinders').val();
+		var endCilinders = $('#endCilinders').val();
+		var randomColor = getRandomColor();
+
+		data.datasets.push({
+				label: "My sece dataset",
+				fillColor: 'transparent',
+				strokeColor: randomColor,
+				pointColor: randomColor,
+				pointStrokeColor: "#fff",
+				pointHighlightFill: "#fff",
+				pointHighlightStroke: randomColor,
+				data: values
+		});
+
+		var ctx = document.getElementById("myChart").getContext("2d");
+		var myLineChart = new Chart(ctx).Line(data, options);
+
+
 		$('.disk-values-for-chart button').each(function() {
+
 			console.log($(this).text());
 		})
 	})
@@ -37,16 +67,6 @@ $(document).ready(function() {
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(220,220,220,1)",
             data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
         }
     ]
 	};
@@ -101,7 +121,4 @@ $(document).ready(function() {
 	    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
 
 	};
-
-	var ctx = document.getElementById("myChart").getContext("2d");
-	var myLineChart = new Chart(ctx).Line(data, options);
 })
